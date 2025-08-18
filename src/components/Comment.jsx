b2500bc35b928d4aa507b0787188e863c6ecb47c
ViniciusBaylet <viniciusbaylet6@gmail.com>
2025-08-18 14:00:08 -0300

    add responsiveness

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -4,7 +4,7 @@ import styles from './Comment.module.css'
 export function Comment() {
     return (
         <div className={styles.comment}>
-            <Avatar src="https://plus.unsplash.com/premium_photo-1689568126014-06fea9d5d341?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+            <Avatar hasBorder={false} src="https://plus.unsplash.com/premium_photo-1689568126014-06fea9d5d341?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
 
             <div className={styles.commentBox}>
                 <div className={styles.commentContent}>
