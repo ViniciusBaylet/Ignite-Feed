57fe73e64295b8c7e0331e2c7baf75dd7cfdf8d2
ViniciusBaylet <viniciusbaylet6@gmail.com>
2025-08-21 18:10:58 -0300

    adicionando imutabilidade ao excluir comentários

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -39,8 +39,11 @@ export function Post({ author, content, publishedAt }) {
         setNewCommentText(event.target.value);
     }
 
-    function deleteComment(comment) {
-
+    function deleteComment(commentToDelete) {
+        const commentsWithoutDeletedOne = comments.filter(comment => {
+            return comment !== commentToDelete;
+        });
+        setComments(commentsWithoutDeletedOne);
     }
 
     return (
